f65b1bac6b38fb47e3039d81c0d34a788386b5ed
dombine <dongbing0207@163.com>
2020-06-06 10:49:12 +0800

    暂时去掉统一登录

--- global/system.go
+++ global/system.go
@@ -1,6 +1,6 @@
 package global
 
 const (
-	SYSTEM_VERSION   = "v1.0"                // system version code
+	SYSTEM_VERSION   = "v1.0.0"              // system version code
 	SYSTEM_COPYRIGHT = "2018 - 2020 dombine" // system copyright
 )
